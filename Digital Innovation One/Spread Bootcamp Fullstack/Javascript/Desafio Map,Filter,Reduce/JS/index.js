1ecbf72dcf9a4d9dc7bbd84e9678095afcc1babd
FelipeEnigmaRock <felj.gomes@gmail.com>
2022-05-18 18:13:43 -0300

    exercicio completo

--- Digital Innovation One/Spread Bootcamp Fullstack/Javascript/Desafio Map,Filter,Reduce/JS/index.js
+++ Digital Innovation One/Spread Bootcamp Fullstack/Javascript/Desafio Map,Filter,Reduce/JS/index.js
@@ -6,3 +6,22 @@ function mapArray (array) {
 const numbers = [6, 66, 666];
 
 console.log(mapArray(numbers));
+
+const abacaxi = {
+    preço: 2
+}
+
+const banana = {
+    preço: 3
+}
+
+function mapThis () {
+    const meuArray = [2, 4, 6];
+
+    return meuArray.map(function (valor){
+        return valor * this.preço;
+    }, banana);
+    
+}
+
+console.log(mapThis());
