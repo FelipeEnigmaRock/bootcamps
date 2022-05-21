195f39e055a160aa90bddfd65049ef7b3301601c
FelipeEnigmaRock <felj.gomes@gmail.com>
2022-05-21 17:02:56 -0300

    Second way

--- Digital Innovation One/Spread Bootcamp Fullstack/Javascript/Desafio Assincrono/src/JS/index.js
+++ Digital Innovation One/Spread Bootcamp Fullstack/Javascript/Desafio Assincrono/src/JS/index.js
@@ -3,15 +3,12 @@ const catImg = document.getElementById('cat');
 const catBtn = document.getElementById('change-cat');
 
 const getCats = async () => {
-    try {
+    
     const data = await fetch(BASE_URL);
     const json = await data.json();
     
     return json.webpurl;
-
-    } catch (e) {
-        console.log(e.message);
-    }
+   
 };
 
 const loadImg = async () => {
